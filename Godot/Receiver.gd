0eeaf012a2e84bd1e80f5952975b8d2d93e700c0
WartedWizard146 <127895652+WartedWizard146@users.noreply.github.com>
2026-03-03 01:36:53 -0600

    Finished Godot project

---
+++ Godot/Receiver.gd
@@ -0,0 +1,7 @@
+extends MeshInstance3D
+
+func OnCalled():
+	print("Receiver: OnCalled received")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
