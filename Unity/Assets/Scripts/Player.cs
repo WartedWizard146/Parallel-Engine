8acdf14d994ed3ec05269115149a2bdc52defa7d
WartedWizard146 <127895652+WartedWizard146@users.noreply.github.com>
2026-04-08 23:50:50 -0500

    added input action to move player

--- Unity/Assets/Scripts/Player.cs
+++ Unity/Assets/Scripts/Player.cs
@@ -1,16 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int health;
+
+    public float speed;
+
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void MovePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
     }
 }
