5b91e15ae09447b34bddb409695c4305dbbc41b1
WartedWizard146 <127895652+WartedWizard146@users.noreply.github.com>
2026-04-09 00:32:25 -0500

    finished player movement

--- Unity/Assets/Scripts/Player.cs
+++ Unity/Assets/Scripts/Player.cs
@@ -16,9 +16,15 @@ public class Player : MonoBehaviour
 
     public void MovePlayer()
     {
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        if(move.sqrMagnitude > 0.1f)
+        {
+            Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+        
     }
     void Start()
     {
